3e8b87e5b642601d3d3771bafce8072089c2fdf3
yoon0yoon <yoon.yoon@hyper-cloud.kr>
2024-06-21 14:55:41 +0900

    1

--- https.py
+++
@@ -1,6 +0,0 @@
-import http.server
-import ssl
-
-httpd = http.server.HTTPServer(('127.0.0.1', 443), http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./cert.pem', server_side=True)
-httpd.serve_forever()
